9a17f846ee34346c7e0c210f8ba0a8e17600cf9a
94adi <adrianrcotuna@gmail.com>
2018-06-13 20:40:54 +0300

    first commit

---
+++ proiect_si/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace proiect_si
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load_image = new System.Windows.Forms.Button();
+            this.load_text = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // load_image
+            // 
+            this.load_image.Location = new System.Drawing.Point(25, 26);
+            this.load_image.Name = "load_image";
+            this.load_image.Size = new System.Drawing.Size(75, 23);
+            this.load_image.TabIndex = 0;
+            this.load_image.Text = "Load image";
+            this.load_image.UseVisualStyleBackColor = true;
+            this.load_image.Click += new System.EventHandler(this.load_image_Click);
+            // 
+            // load_text
+            // 
+            this.load_text.Location = new System.Drawing.Point(25, 64);
+            this.load_text.Name = "load_text";
+            this.load_text.Size = new System.Drawing.Size(75, 23);
+            this.load_text.TabIndex = 1;
+            this.load_text.Text = "Load text";
+            this.load_text.UseVisualStyleBackColor = true;
+            this.load_text.Click += new System.EventHandler(this.load_text_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(457, 118);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(373, 312);
+            this.textBox1.TabIndex = 3;
+            // 
+            // encrypt
+            // 
+            this.encrypt.Location = new System.Drawing.Point(25, 452);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(75, 23);
+            this.encrypt.TabIndex = 4;
+            this.encrypt.Text = "Encrypt";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(134, 452);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(75, 23);
+            this.decrypt.TabIndex = 5;
+            this.decrypt.Text = "Decrypt";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(422, 452);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 6;
+            this.save.Text = "Save text";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(25, 118);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(346, 312);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(346, 312);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 522);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.load_text);
+            this.Controls.Add(this.load_image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load_image;
+        private System.Windows.Forms.Button load_text;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ proiect_si/Form1.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proiect_si
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+        private string readcontent; //continutul fisierului
+        private string readpath; //
+        private Bitmap image, img,dec_img; //image displays the unmodified picture while img displays the image after the message was hidden
+        static int[] binary = new int[8]; //holds the binary rep of the latest elem from byte_array
+        static byte[] byte_array; //holds the ASCII rep for the loaded text
+        private int w, h; //width & height of the image
+        private Color pink; //access the RGB properties of each pixel
+        private int contor_array = 0;
+        private int contor_binar = 0;
+        private int w_char = 0; //counter used to keep track of # char/line
+        private int char_line = 0; //number of characters per line
+         //coordonate pt parcurgerea imaginii
+        /***************************Functii conversie*******************/
+        public static byte[] StringToByteArray(string s)
+        {
+            return CharArrayToByteArray(s.ToCharArray());
+        }
+        public static byte[] CharArrayToByteArray(char[] array)
+        {
+            return Encoding.ASCII.GetBytes(array, 0, array.Length);
+        }
+        public static string ByteArrayToString(byte[] array)
+        {
+            return Encoding.ASCII.GetString(array);
+        }
+        public static byte Binary_to_Byte(string s)
+        {          
+                byte dec = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[s.Length - i - 1] == '0') continue;
+                    dec += (byte)Math.Pow(2, i);
+                }
+                return dec;
+        }
+        public static int get_LSB(byte x)
+        {
+            int[] rep = new int[8];
+            string s = Convert.ToString(x, 2);
+            rep = s.PadLeft(8, '0').Select(c => int.Parse(c.ToString())).ToArray();
+            return rep[7];
+        }
+        public static void Byte_to_binary(byte x)
+        {
+            string s = Convert.ToString(x, 2);
+            binary = s.PadLeft(8, '0').Select(c => int.Parse(c.ToString())).ToArray();
+        }
+        /*******************EVENT HANDLERS**************/
+        private void load_text_Click(object sender, EventArgs e)
+        {
+            readcontent = String.Empty;
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Text|*.txt";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                readpath = opf.FileName;
+                StreamReader sr = new StreamReader(readpath, false);
+                readcontent += sr.ReadToEnd();
+                sr.Close();
+            }
+            byte_array = StringToByteArray(readcontent);
+            array_length = byte_array.Length;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if(text_decriptat.Length > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Text Document|*.txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter sw = new StreamWriter(sfd.FileName, true);
+                    sw.WriteLine(text_decriptat);
+                    sw.Close();
+                }
+            }
+        }
+        private int pixel_contor = 0;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int array_length;
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            int width = -1, height = 0;
+            int r_LSB, g_LSB, b_LSB;
+           // bool b_minus_red = false, b_plus_red = false, b_minus_green = false, b_plus_green = false, b_minus_blue = false, b_plus_blue = false;
+            byte red,green,blue;
+            //byte minus_red, plus_red, minus_green, plus_green, minus_blue, plus_blue;
+            char_line = img.Width / 3; //number of characters/line
+            while(contor_array < array_length) //cat timp mai sunt caractere
+            {
+                Byte_to_binary(byte_array[contor_array]); //conversie in binar (int array global)
+                contor_array++; 
+                pixel_contor = 0;
+                contor_binar = 0;
+                if(w_char == char_line) //daca nr de char/linie depaseste limita admisa
+                {
+                    //trecere pe linie noua si reinit width
+                    w_char = 0;
+                    height++; //new line
+                    width = -1;
+                }
+                else
+                {
+                    w_char++; // 1 x w_char = 3 pixels
+                }
+                while(pixel_contor <= 2) //0,1,2
+                { //width to be incremented at the end
+                    if (width == img.Width) width = 0;
+                    if(pixel_contor == 0 || pixel_contor == 1)
+                    {
+                        width++;
+                                               
+                        pink = img.GetPixel(width, height);
+                        red = pink.R;
+                        green = pink.G;
+                        blue = pink.B;
+                        r_LSB = get_LSB(red);
+                        g_LSB = get_LSB(green);
+                        b_LSB = get_LSB(blue);
+                        if (pixel_contor == 1) { contor_binar = 3; }                       
+                        //contor_binar e pe 0
+                        if ((r_LSB == 1 && binary[contor_binar] == 1) || (r_LSB == 0 && binary[contor_binar] == 0))
+                        {
+
+                        }
+                        if (r_LSB == 1 && binary[contor_binar] == 0)
+                        {
+                           img.SetPixel(width, height, Color.FromArgb(255, --red, pink.G, pink.B));
+                        }
+                        if (r_LSB == 0 && binary[contor_binar] == 1)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, ++red, pink.G, pink.B));
+                        }
+                        /*************************************************************************/
+                        contor_binar++; //1 //4
+                        if ((g_LSB == 1 && binary[contor_binar] == 1) || (g_LSB == 0 && binary[contor_binar] == 0))
+                        {
+                        }
+                        if (g_LSB == 1 && binary[contor_binar] == 0)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, red, --green, pink.B));
+                        }
+                        if (g_LSB == 0 && binary[contor_binar] == 1)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, red, ++green, pink.B));
+                        }
+                        /*************************************************************************/
+                        contor_binar++; //2 //5
+                        if ((b_LSB == 1 && binary[contor_binar] == 1) || (b_LSB == 0 && binary[contor_binar] == 0))
+                        {
+                        }
+               
+                        if (b_LSB == 1 && binary[contor_binar] == 0)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, red, green, --blue));
+                        }
+                        if (b_LSB == 0 && binary[contor_binar] == 1)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, red, green, ++blue));
+                        }
+                        }
+                    if (pixel_contor == 2)
+                    {
+                        width++;                       
+                        pink = img.GetPixel(width, height);
+                        red = pink.R;
+                        green = pink.G;
+                        r_LSB = get_LSB(red);
+                        g_LSB = get_LSB(green);
+                        contor_binar = 6;
+                        if ((r_LSB == 1 && binary[contor_binar] == 1) || (r_LSB == 0 && binary[contor_binar] == 0))
+                        {
+
+                        }
+                        if (r_LSB == 1 && binary[contor_binar] == 0)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, --red, green, pink.B));
+
+                        }
+                        if (r_LSB == 0 && binary[contor_binar] == 1)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, ++red, green, pink.B));
+
+                        }
+                        /*************************************************************************/
+                        contor_binar = 7; //7 END
+                        if ((g_LSB == 1 && binary[contor_binar] == 1) || (g_LSB == 0 && binary[contor_binar] == 0))
+                        {
+
+                        }
+                        if (g_LSB == 1 && binary[contor_binar] == 0)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, red, --green, pink.B));
+
+                        }
+                        if (g_LSB == 0 && binary[contor_binar] == 1)
+                        {
+                            img.SetPixel(width, height, Color.FromArgb(255, red, ++green, pink.B));
+
+                        }
+                    }
+                    pixel_contor++;
+                }                    
+                }
+            SaveFileDialog sfd = new SaveFileDialog();
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                img.Save(sfd.FileName + "_" + array_length.ToString() + ".bmp");
+            }
+            }
+        /*while(contor_array < byte_array.Length)
+        {
+            width = 0;
+            contor_binar = 0;
+            Byte_to_binary(byte_array[contor_array]);
+            while(width < 8)
+            {
+                pink = img.GetPixel(width, height);
+                red = pink.R;
+                LSB = get_LSB(red);
+                if((LSB == 1 && binary[contor_binar] == 1) || (LSB == 0 && binary[contor_binar] == 0))
+                {
+                    img.SetPixel(width, height, Color.FromArgb(255,pink.R, pink.G, pink.B));
+                }
+                if(LSB == 0 && binary[contor_binar] == 1)
+                {
+                    img.SetPixel(width, height, Color.FromArgb(255, ++red, pink.G, pink.B));
+                }
+                if(LSB == 1 && binary[contor_binar] == 0)
+                {
+                    img.SetPixel(width, height, Color.FromArgb(255, --red, pink.G, pink.B));
+                }
+                width++;
+                contor_binar++;
+            }
+            contor_array++;
+            height++;
+        }
+        SaveFileDialog sfd = new SaveFileDialog();
+        if(sfd.ShowDialog() == DialogResult.OK)
+        img.Save(sfd.FileName + "_"+ height.ToString() + ".bmp");
+        textBox1.Clear();
+        MessageBox.Show("SUCCESS");*/
+        string text_decriptat = "";
+        int contor_decript = 0;
+        byte[] decrypt_array;
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            int nr_char_codate;
+            int nr_linii;
+            int contor_char = 0;
+            int contor_linie = 0;
+            int contor_width = 0;
+            int d_pixel_contor = 0;
+            int nr_char_linie;
+            int d_width = 0;
+            int d_height = 0;
+            int w, h;
+            string[] s;
+            string[] s1;
+            string sec_bin = "";
+            Color mycolor;
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "(*.bmp)|*.bmp";
+            if(opf.ShowDialog() == DialogResult.OK)
+            {
+                dec_img = (Bitmap)Image.FromFile(opf.FileName);
+                w = dec_img.Width;
+                h = dec_img.Height;
+                s = opf.FileName.ToString().Split('_');
+                s1 = s[1].Split('.');
+                nr_char_codate = int.Parse(s1[0]);
+                nr_char_linie = img.Width / 3;
+                nr_linii = nr_char_codate / nr_char_linie;
+                decrypt_array = new byte[nr_char_codate+1];
+                while (contor_char < nr_char_codate)
+                {
+                    d_pixel_contor = 0;
+                    contor_char++;
+                    if(contor_width >= nr_char_linie)
+                    {
+                        contor_width = 0;
+                        d_width = 0;
+                        d_height++;
+                    }
+                    else
+                    {
+                        contor_width++;
+                    }
+                    while(d_pixel_contor <= 2)
+                    {
+                        if(d_pixel_contor == 0 || d_pixel_contor == 1)
+                        {
+                            mycolor = dec_img.GetPixel(d_width, d_height);
+                            d_width++;
+                            sec_bin += get_LSB(mycolor.R);
+                            sec_bin += get_LSB(mycolor.G);
+                            sec_bin += get_LSB(mycolor.B);
+                            d_pixel_contor++;
+                        }
+                        if(d_pixel_contor == 2)
+                        {
+                            mycolor = dec_img.GetPixel(d_width, d_height);
+                            d_width++;
+                            sec_bin += get_LSB(mycolor.R);
+                            sec_bin += get_LSB(mycolor.G);
+                            decrypt_array[contor_decript] = Binary_to_Byte(sec_bin);
+                            contor_decript++;
+                            sec_bin = "";
+                            d_pixel_contor++;
+                        }
+                    }                  
+                }
+            }
+            MessageBox.Show("SUCCESS!");
+            textBox1.Clear();
+            textBox1.Text = ByteArrayToString(decrypt_array);
+            /*int img_h;
+            int m;
+            int he = 0;
+            int w = 0;
+            string[] s, s1;
+
+            string sec_binara = "";
+            OpenFileDialog opf = new OpenFileDialog();
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                dec_img = (Bitmap)Image.FromFile(opf.FileName);
+                pictureBox1.Image = dec_img;
+                s = opf.FileName.ToString().Split('_');
+                s1 = s[1].Split('.');
+                img_h = int.Parse(s1[0]);
+                byte[] decript_array = new byte[img_h + 1];
+                while (he < img_h)
+                {
+                    w = 0;
+                    while (w < 8)
+                    {
+                        m = get_LSB(dec_img.GetPixel(w, he).R);
+                        sec_binara += m.ToString();
+                        w++;
+                    }
+                    decript_array[he] = Binary_to_Byte(sec_binara);
+                    sec_binara = "";
+                    he++;
+                }
+                text_decriptat = ByteArrayToString(decript_array);
+                textBox1.Text = text_decriptat;
+
+            }*/
+        }
+        private void load_image_Click(object sender, EventArgs e)
+        {
+            int words;
+            string s_words;
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.FilterIndex = 1;
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                image = (Bitmap)Image.FromFile(opf.FileName);
+                img = (Bitmap)Image.FromFile(opf.FileName);
+                pictureBox1.Image = image;
+                w = img.Width;
+                h = img.Height;
+                words = h * (w / 3);
+                s_words = words.ToString();
+                MessageBox.Show("# of chars:" + s_words);
+                //MessageBox.Show("Width: " + w.ToString());
+               // MessageBox.Show("Height: " + h.ToString());
+            }
+        }
+    }
+
+}
